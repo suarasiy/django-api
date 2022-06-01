cdb419cdcc80d3e7d340744be017ef93a3dd792a
suarasiy <sidiqindrajatiyusuf@gmail.com>
2022-06-01 13:59:04 +0800

    Token authentication

--- py_client/list.py
+++ py_client/list.py
@@ -1,6 +1,24 @@
 import requests
+from getpass import getpass
 
+
+auth_endpoint = "http://localhost:8000/api/auth/"
 endpoint = "http://localhost:8000/api/products/"
 
-get_result = requests.get(endpoint)
-print(get_result.json())
+username = input("Username: ")
+password = getpass()
+
+auth_response = requests.post(auth_endpoint, json={
+    "username": username,
+    "password": password,
+})
+
+print(auth_response.json())
+
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    get_result = requests.get(endpoint, headers=headers)
+    print(get_result.json())
