47ce0222096c85036195fd9250590e623ef954ed
suarasiy <sidiqindrajatiyusuf@gmail.com>
2022-06-04 23:01:22 +0800

    Pagination

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -17,7 +17,6 @@ class ProductInlineSerializer(serializers.Serializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     owner = UserPublicSerializer(source='user', read_only=True)
-    my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(
         view_name='product-detail',
@@ -25,8 +24,6 @@ class ProductSerializer(serializers.ModelSerializer):
     )
     title = serializers.CharField(
         validators=[validate_title_no_hello, unique_product_title])
-    related_product = ProductInlineSerializer(
-        source='user.product_set.all', many=True, read_only=True)
 
     class Meta:
         model = Product
@@ -39,37 +36,7 @@ class ProductSerializer(serializers.ModelSerializer):
             'content',
             'price',
             'sale_price',
-            'my_discount',
-            'related_product'
         ]
-
-    # def validate_title(self, value):
-    #     request = self.context.get('request')
-    #     user = request.user
-    #     print(user)
-    #     qs = Product.objects.filter(user=user, title__iexact=value)
-    #     if qs.exists():
-    #         raise serializers.ValidationError(
-    #             f"{value} is already a product name.")
-    #     return value
-
-    # def create(self, validated_data):
-    #     # return Product.objects.create(**validated_data)
-    #     # email = validated_data.pop('email')
-    #     obj = super().create(validated_data)
-    #     # print(email, obj)
-    #     return obj
-
-    # def update(self, instance, validated_data):
-    #     email = validated_data.pop('email')
-    #     return super().update(instance, validated_data)
-
-    def get_my_discount(self, obj):
-        if not hasattr(obj, 'id'):
-            return None
-        if not isinstance(obj, Product):
-            return None
-        return obj.get_discount()
 
     def get_edit_url(self, obj):
         request = self.context.get('request')
