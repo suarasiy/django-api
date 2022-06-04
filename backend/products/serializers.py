d10aa9f74880d227c9b5a7dfa08e77bc50f22720
suarasiy <sidiqindrajatiyusuf@gmail.com>
2022-06-05 01:20:28 +0800

    A django based search for our product api

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -36,6 +36,7 @@ class ProductSerializer(serializers.ModelSerializer):
             'content',
             'price',
             'sale_price',
+            'public'
         ]
 
     def get_edit_url(self, obj):
