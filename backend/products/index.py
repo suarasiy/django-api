ff59e2b2ebd64630132861bf5cb9889b6d2bed7f
suarasiy <sidiqindrajatiyusuf@gmail.com>
2022-06-08 10:00:25 +0800

    Algolia search client

--- backend/products/index.py
+++ backend/products/index.py
@@ -6,7 +6,7 @@ from .models import Product
 
 @register(Product)
 class ProductIndex(AlgoliaIndex):
-    should_index = 'is_public'
+    # should_index = 'is_public'
     fields = [
         'title',
         'content',
@@ -14,4 +14,8 @@ class ProductIndex(AlgoliaIndex):
         'user',
         'public'
     ]
+    settings = {
+        'searchableAttributes': ['title', 'content', 'price'],
+        'attributesForFaceting': ['user', 'public']
+    }
     tags = 'get_tags_list'
