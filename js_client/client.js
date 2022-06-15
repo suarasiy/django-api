b63608f9374de7ca85c7feaeace9528d95b7ddf2
suarasiy <sidiqindrajatiyusuf@gmail.com>
2022-06-15 13:19:21 +0800

    Using JWT with js client

--- js_client/client.js
+++ js_client/client.js
@@ -1,5 +1,6 @@
 const BASE_ENDPOINT = 'http://localhost:8000/api';
 const loginForm = document.querySelector('#login-form');
+const contentContainer = document.querySelector('#content-container');
 
 if (loginForm) loginForm.addEventListener('submit', handleLogin);
 
@@ -20,13 +21,119 @@ function handleLogin(event) {
   };
   fetch(loginEndpoint, options)
     .then((response) => {
-      console.log(response);
       return response.json();
     })
-    .then((x) => {
-      console.log(x);
+    .then((authData) => {
+      handleAuthData(authData, getProductList);
     })
     .catch((error) => {
       console.log(error);
     });
 }
+
+function refreshJWT() {
+  const endpoint = `${BASE_ENDPOINT}/token/refresh/`;
+  const refresh = localStorage.getItem('refresh');
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      refresh,
+    }),
+  };
+
+  fetch(endpoint, options)
+    .then((response) => response.json())
+    .then((data) => {
+      handleAuthData(data, getProductList);
+    })
+    .catch((error) => {
+      console.error('error occured: ', error);
+    });
+}
+
+function verifyJWT() {
+  console.log('verifying...');
+  const endpoint = `${BASE_ENDPOINT}/token/verify/`;
+  const token = localStorage.getItem('access');
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      token,
+    }),
+  };
+
+  fetch(endpoint, options)
+    .then((response) => {
+      console.log('one: ', response);
+      if (response.status === 200) {
+        console.info('Token active.');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.info('data', data);
+      if (data.code && data.code === 'token_not_valid') refreshJWT();
+    })
+    .catch((error) => {
+      console.error(error);
+      alert('token rejected. please login again!');
+    });
+}
+
+verifyJWT();
+
+function handleAuthData(authData, callback) {
+  localStorage.setItem('access', authData.access);
+  localStorage.setItem('refresh', authData.refresh);
+  if (callback) {
+    callback();
+  }
+}
+
+function writeToContent(data) {
+  if (contentContainer) {
+    contentContainer.innerHTML = '<pre>' + JSON.stringify(data, null, 4) + '</pre>';
+  }
+}
+
+function getFetchOption(method, token, body) {
+  return {
+    method: method === null ? 'GET' : method,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: body ? body : null,
+  };
+}
+
+function tokenIsNotValid(jsonData) {
+  if (jsonData.code && jsonData.code === 'token_not_valid') {
+    return false;
+  }
+  return true;
+}
+
+function getProductList() {
+  const endpoint = `${BASE_ENDPOINT}/products/`;
+  const token = localStorage.getItem('access');
+  const options = getFetchOption('GET', token);
+
+  fetch(endpoint, options)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const validData = tokenIsNotValid(data);
+      if (validData) {
+        writeToContent(data);
+      }
+    });
+}
+getProductList();
